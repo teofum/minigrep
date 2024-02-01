6dc747384345fd27d5bd1f3bf9ad54f8cfa00093
Teo Fumagalli <teo.fumagalli@aerolab.co>
2024-02-01 18:49:57 -0300

    refactor: move code out of main and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,40 @@
 use std::env;
+use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::from_args(&args).unwrap_or_else(|err| {
+        println!("Unable to parse arguments: {err}");
+        println!("Usage: minigrep <query> <file_path>");
+        process::exit(1);
+    });
 
-    let query = match args.get(1) {
-        Some(arg) => arg,
-        None => {
-            println!("Must specify query. Usage: minigrep <query> <file_path>");
-            return;
+    println!("Searching for {} in {}", config.query, config.file_path);
+
+    run(config);
+}
+
+fn run(config: Config) {
+    let contents = fs::read_to_string(config.file_path).expect("Should be able to read the file");
+
+    println!("File contents:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn from_args(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
         }
-    };
 
-    let file_path = match args.get(2) {
-        Some(arg) => arg,
-        None => {
-            println!("Must specify file path. Usage: minigrep <query> <file_path>");
-            return;
-        }
-    };
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    println!("Searching for {query} in {file_path}");
+        Ok(Config { query, file_path })
+    }
 }
