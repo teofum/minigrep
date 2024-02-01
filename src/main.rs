9baa565ed8440104ef41e8cc594668390826277b
Teo Fumagalli <teo.fumagalli@aerolab.co>
2024-02-01 17:02:12 -0300

    feat: read command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = match args.get(1) {
+        Some(arg) => arg,
+        None => {
+            println!("Must specify query. Usage: minigrep <query> <file_path>");
+            return;
+        }
+    };
+
+    let file_path = match args.get(2) {
+        Some(arg) => arg,
+        None => {
+            println!("Must specify file path. Usage: minigrep <query> <file_path>");
+            return;
+        }
+    };
+
+    println!("Searching for {query} in {file_path}");
 }
