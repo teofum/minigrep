8b69aacd8d377a331b309f8b3ce644616fbebe0d
Teo Fumagalli <teo.fumagalli@aerolab.co>
2024-02-05 12:59:16 -0300

    fix: print errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::from_args(&args).unwrap_or_else(|err| {
-        println!("Unable to parse arguments: {err}");
-        println!("Usage: minigrep <query> <file_path>");
+        eprintln!("Unable to parse arguments: {err}");
+        eprintln!("Usage: minigrep <query> <file_path>");
         process::exit(1);
     });
 
     if let Err(err) = minigrep::run(config) {
-        println!("Error: {err}");
+        eprintln!("Error: {err}");
         process::exit(1);
     };
 }
